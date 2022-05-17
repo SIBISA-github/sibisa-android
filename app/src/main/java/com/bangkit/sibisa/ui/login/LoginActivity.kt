b9e2187bb158d31753c038fa261e095f449ecaa4
fakhrinail <fakhrinailwbw@gmail.com>
2022-05-17 21:11:40 +0700

    add login page

--- app/src/main/java/com/bangkit/sibisa/ui/login/LoginActivity.kt
+++ app/src/main/java/com/bangkit/sibisa/ui/login/LoginActivity.kt
@@ -2,10 +2,16 @@ package com.bangkit.sibisa.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bangkit.sibisa.R
+import com.bangkit.sibisa.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
 }
