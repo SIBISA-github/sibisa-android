824c585ee87a3d867c68e503045e3afab9bf1159
fakhrinail <fakhrinailwbw@gmail.com>
2022-05-25 16:16:16 +0700

    change notif fragment to profile

--- app/src/main/java/com/bangkit/sibisa/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/bangkit/sibisa/ui/profile/ProfileViewModel.kt
@@ -1,10 +1,10 @@
-package com.bangkit.sibisa.ui.notifications
+package com.bangkit.sibisa.ui.profile
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
