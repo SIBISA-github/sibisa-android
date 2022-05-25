0274917134dde83ac44496bbd9fd256488b19889
fakhrinail <fakhrinailwbw@gmail.com>
2022-05-25 16:23:25 +0700

    fix import error

--- app/src/main/java/com/bangkit/sibisa/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/bangkit/sibisa/ui/profile/ProfileFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.bangkit.sibisa.databinding.FragmentNotificationsBinding
+import com.bangkit.sibisa.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentProfileBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class ProfileFragment : Fragment() {
         val notificationsViewModel =
             ViewModelProvider(this).get(ProfileViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
