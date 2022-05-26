2b5c03608dbb903b3991d8bd3e1f04a4f5442fd5
fakhrinail <fakhrinailwbw@gmail.com>
2022-05-26 23:29:45 +0700

    modify login response

--- app/src/main/java/com/bangkit/sibisa/models/login/LoginResponse.kt
+++ app/src/main/java/com/bangkit/sibisa/models/login/LoginResponse.kt
@@ -1,5 +1,8 @@
 package com.bangkit.sibisa.models.login
 
+import com.google.gson.annotations.SerializedName
+
 data class LoginResponse(
+    @field:SerializedName("token")
     val token: String? = null
 )
