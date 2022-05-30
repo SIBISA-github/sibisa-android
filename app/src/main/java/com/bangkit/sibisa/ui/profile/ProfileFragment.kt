ba9a405944d1c4a1d72c6632a00313fe4974332d
fakhrinail <fakhrinailwbw@gmail.com>
2022-05-30 15:15:54 +0700

    add logout

--- app/src/main/java/com/bangkit/sibisa/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/bangkit/sibisa/ui/profile/ProfileFragment.kt
@@ -1,11 +1,15 @@
 package com.bangkit.sibisa.ui.profile
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bangkit.sibisa.R
 import com.bangkit.sibisa.databinding.FragmentProfileBinding
+import com.bangkit.sibisa.pref.UserPreference
+import com.bangkit.sibisa.ui.login.LoginActivity
 
 class ProfileFragment : Fragment() {
 
@@ -31,7 +35,7 @@ class ProfileFragment : Fragment() {
                 true
             }
             R.id.logout_menu -> {
-                // logout
+                showDialog()
                 true
             }
             else -> super.onOptionsItemSelected(item)
@@ -54,5 +58,31 @@ class ProfileFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun logout() {
+        val pref = UserPreference(requireContext())
+        pref.clearUser()
+
+        startActivity(Intent(requireContext(), LoginActivity::class.java))
+        activity?.finish()
+    }
+
+    private fun showDialog() {
+        val builder: AlertDialog.Builder? = activity?.let {
+            AlertDialog.Builder(it)
+        }
+
+        builder?.setMessage(R.string.logout_dialog_message)?.setPositiveButton(
+            R.string.logout_dialog_message_yes
+        ) { dialog, _ ->
+            dialog.dismiss()
+            logout()
+        }?.setNegativeButton(R.string.logout_dialog_message_no) { dialog, _ ->
+            dialog.cancel()
+        }
+
+        val dialog: AlertDialog? = builder?.create()
+        dialog?.show()
     }
 }
