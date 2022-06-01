8baa293a6ac3c0c2bba8fac87cdc7005c74bd55d
fakhrinail <fakhrinailwbw@gmail.com>
2022-06-01 14:27:15 +0700

    add quiz page

--- app/src/main/java/com/bangkit/sibisa/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bangkit/sibisa/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.bangkit.sibisa.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bangkit.sibisa.databinding.FragmentHomeBinding
+import com.bangkit.sibisa.ui.quiz.QuizActivity
 
 class HomeFragment : Fragment() {
 
@@ -31,6 +33,10 @@ class HomeFragment : Fragment() {
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
+        }
+
+        binding.quizButton.setOnClickListener {
+            startActivity(Intent(requireContext(), QuizActivity::class.java))
         }
         return root
     }
